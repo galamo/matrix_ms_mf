12fd8a2cc938394632ca81f000f3e81a21b9fe0e
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-10-26 19:06:25 +0300

    nodemon

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,7 +4,13 @@ import express from "express"
 const app = express();
 
 app.get("/health-check", (req, res) => {
-    res.json({ message: "Server is Running" })
+    res.json({ message: `Server is Running ${new Date().toString()}` })
+})
+
+app.get("/countries", (req, res) => {
+    for (let index = 0; index < 9999999999; index++) {
+    }
+    res.json({ countries: ["Israel", "Italy", "England"] })
 })
 
 app.listen(port)
