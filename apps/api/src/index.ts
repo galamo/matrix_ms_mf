945cb23496d8d1c9f271c64a0797c26c2abec552
Gal Amouyal <galamouy@Gals-MacBook-Pro.local>
2022-10-26 18:52:40 +0300

    api

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,4 +1,10 @@
 const port: number = 4000;
+import express from "express"
 
+const app = express();
 
-console.log(port)
+app.get("/health-check", (req, res) => {
+    res.json({ message: "Server is Running" })
+})
+
+app.listen(port)
