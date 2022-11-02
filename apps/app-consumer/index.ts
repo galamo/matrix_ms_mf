eedb7313447b9f57845274ed689f051bee4209dd
Gal Amouyal <galamouy@gals-mbp.givatayim.il.ibm.com>
2022-11-02 17:34:47 +0200

    ttt

--- apps/app-consumer/index.ts
+++ apps/app-consumer/index.ts
@@ -1,13 +1,6 @@
-import axios from "axios";
 console.log("Consumer is up and running")
 console.log("Consumer is up and running")
 console.log("Consumer is up and running")
 console.log("Consumer is up and running")
 console.log("Consumer is up and running")
 
-async function consumeDataApi() {
-    const result = await axios.get("http://localhost:4000")
-    console.log(result.data)
-}
-
-consumeDataApi();
