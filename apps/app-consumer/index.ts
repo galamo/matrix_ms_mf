8ffae424a57cd21c0efabee8309e9ecc3bc720c2
Gal Amouyal <galamouy@gals-mbp.givatayim.il.ibm.com>
2022-11-02 18:57:34 +0200

    docker

--- apps/app-consumer/index.ts
+++
@@ -1,6 +0,0 @@
-console.log("Consumer is up and running")
-console.log("Consumer is up and running")
-console.log("Consumer is up and running")
-console.log("Consumer is up and running")
-console.log("Consumer is up and running")
-
---
+++ apps/app-consumer/src/index.ts
@@ -0,0 +1,5 @@
+function init() {
+    console.log("Running JS in docker for Linor");
+}
+
+init();
